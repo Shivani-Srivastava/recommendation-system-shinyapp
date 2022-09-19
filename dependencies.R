1c61d05198eb26d80738e4536dc31b5a8a655964
Sudhir Voleti <sudhir_voleti@isb.edu>
2022-09-19 11:45:18 +0530

    Update dependencies.R

--- dependencies.R
+++ dependencies.R
@@ -1,6 +1,6 @@
 if (!require(shinyWidgets)) {install.packages("shinyWidgets")};  library(shinyWidgets)
 if (!require(shiny)) {install.packages("shiny")};  library(shiny)
-#if (!require(quanteda)) {install.packages("quanteda")};  library(quanteda)
+if (!require(quanteda)) {install.packages("quanteda")};  library(quanteda)
 if (!require(ggplot2)) {install.packages("ggplot2")};  library(ggplot2)
 if (!require(magrittr)) {install.packages("magrittr")};  library(magrittr)
 if (!require(text2vec)) {install.packages("text2vec")};  library(text2vec)
